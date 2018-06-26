542edb3766a559ad66a1a81e0816551ba62cd51f
magda-korzeniowska <m.korzeniowska@tlen.pl>
2018-06-26 16:36:25 +0200

    Add 'have at least one entry within feed container' spec to 'Initial Entries' test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -88,10 +88,14 @@ $(function() {
             icon.click();
             expect(body.classList.contains('menu-hidden')).toBe(true);
           });
-
     });
 
+    /*
+     * "Initial Entries" test suite
+     */
+
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -99,6 +103,18 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+           loadFeed(0, function() {
+             done();
+           });
+         });
+         
+         it('have at least one entry within feed container', function() {
+           const feedEntries = document.querySelector('.feed').querySelectorAll('.entry');
+
+           expect(feedEntries.length).toBeGreaterThan(0);
+         });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
